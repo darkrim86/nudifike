7971489fe3cf31a3b972f7414d588df807e22e8b
darkrim86 <darkrim86@gmail.com>
2025-12-07 12:48:48 -0300

    correçãoo pixel + checkout utm params

--- checkout.js
+++ checkout.js
@@ -1,7 +1,25 @@
-var checkoutBtnC = document.getElementById('checkout-btn');
-checkoutBtnC.addEventListener('click', processCheckout);
+    
+    var checkoutBtnC = document.getElementById('checkout-btn');
+    checkoutBtnC.addEventListener('click', processCheckout);
 
-function processCheckout() {
-    localStorage.setItem('final', 'true');
-    window.location.href = 'https://pay.risepay.com.br/Pay/d65701e2faf74363a3547be700df690d';
-}
+    function processCheckout() {
+
+        // URL do checkout
+        let baseURL = 'https://pay.risepay.com.br/Pay/d65701e2faf74363a3547be700df690d';
+        
+        // Captura UTMs da URL atual
+        let params = window.location.search;
+
+        // Dispara InitiateCheckout do Meta Pixel
+        try {
+            fbq('track', 'InitiateCheckout');
+        } catch(e) {
+            console.warn("Pixel não carregou, seguindo mesmo assim.");
+        }
+
+        // Apenas marca que o usuário chegou aqui (se você quiser usar depois)
+        localStorage.setItem('final', 'true');
+
+        // Redireciona já com UTMs coladas
+        window.location.href = baseURL + params;
+    }
